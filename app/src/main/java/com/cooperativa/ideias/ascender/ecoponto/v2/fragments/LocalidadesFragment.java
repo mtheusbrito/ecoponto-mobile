b6e929c1c43364f8e73f7cd5767c21f794ef1f48
LucasRobot <email.lucasdacaosta@gmail.com>
2019-06-13 17:09:21 -0300

    FragmentLocalidades com metodo AdView

--- app/src/main/java/com/cooperativa/ideias/ascender/ecoponto/v2/fragments/LocalidadesFragment.java
+++ app/src/main/java/com/cooperativa/ideias/ascender/ecoponto/v2/fragments/LocalidadesFragment.java
@@ -1,5 +1,6 @@
 package com.cooperativa.ideias.ascender.ecoponto.v2.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,38 +9,93 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import com.cooperativa.ideias.ascender.ecoponto.R;
 import com.cooperativa.ideias.ascender.ecoponto.v2.DAO.ConfiguracoesFirebase;
 import com.cooperativa.ideias.ascender.ecoponto.v2.adapters.LocalidadesAdapter;
 import com.cooperativa.ideias.ascender.ecoponto.v2.models.Ponto;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+
 import java.util.ArrayList;
 
-public class LocalidadesFragment extends Fragment implements OnBackPressed{
+import static android.content.ContentValues.TAG;
+
+public class LocalidadesFragment extends Fragment implements OnBackPressed {
     private RecyclerView recyclerView;
     private ArrayList<Ponto> pontos;
     private LocalidadesAdapter adapter;
     private ProgressBar progressBar;
 
+    //Variaveis para AdMobi
+    private AdView adView;
+    private   String TESTDEVICE_SGS5;
+    private   String TESTDEVICE_SGS3MINI;
+    private   String TESTDEVICE_DUOS_Y;
+
+
+
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.v2_localidades_fragment, container, false);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+        ((AppCompatActivity) getActivity()).getSupportActionBar().show();
         getActivity().setTitle("Localidades");
         initView(view);
         preencherLista();
         return view;
+
     }
+    private void showGoogleAdMobAds(Activity activity) {
+
+        try {
+            AdView adView = new AdView(activity);
+            adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+            adView.setAdSize(AdSize.BANNER);
+
+            LinearLayout layout = (LinearLayout) activity.findViewById(R.id.adView);
+            // Add the adView to it
+            layout.addView(adView);
+            // Initiate a generic request.
+            AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .addTestDevice(TESTDEVICE_SGS5)
+                    .addTestDevice(TESTDEVICE_SGS3MINI)
+                    .addTestDevice(TESTDEVICE_DUOS_Y).build();
+            // Load the adView with the ad request.
+            adView.loadAd(adRequest);
+        } catch (Exception e) {
+            Log.e(TAG, "Error while getting ads", e);
+        }
+    }
+
+   // public void AdMobi(View view){
+     //   initView(view);
+      //  adView = new AdView(getActivity());
+       // adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+       // adView.setAdSize(AdSize.BANNER);
+      // LinearLayout layout = (LinearLayout)view.findViewById(R.id.adView);
+      //  AdView mAdView = (AdView) view.findViewById(R.id.adView);
+      //  mAdView.addView(adView);
+       // adView.addView(adView);
+       // AdRequest adRequest = new AdRequest.Builder().build();
+      ////  adView.loadAd(adRequest);
+      //  return view;
+
+   // }
 
     private void preencherLista() {
         pontos = new ArrayList<>();
@@ -99,4 +155,8 @@ public class LocalidadesFragment extends Fragment implements OnBackPressed{
     public void onBackPressed() {
 
     }
+
+
+
 }
+
